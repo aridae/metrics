089c198a7eb4257706dc718a4d189348f7a4f097
aridae <rusinovadaria185@gmail.com>
2024-09-30 21:30:36 +0300

    post iter5: tiding up after refactor

--- internal/server/transport/http/handlers/_stub/controller.go
+++ internal/server/transport/http/handlers/_stub/controller.go
@@ -2,12 +2,13 @@ package _stub
 
 import (
 	"context"
+	metricsupsertstrategies "github.com/aridae/go-metrics-store/internal/server/metrics-upsert-strategies"
 	"github.com/aridae/go-metrics-store/internal/server/models"
 )
 
 type ControllerNoErrStub struct{}
 
-func (stub *ControllerNoErrStub) UpsertScalarMetric(_ context.Context, _ models.ScalarMetricToRegister) error {
+func (stub *ControllerNoErrStub) UpsertScalarMetric(ctx context.Context, metricToRegister models.ScalarMetricToRegister, strategy metricsupsertstrategies.Strategy) error {
 	return nil
 }
 
